92588ccd21c163aa1b71af623c9b310d3f8faca5
OleksandrKuchera <leato44eek@gmail.com>
2024-03-13 22:04:53 +0200

    fixed errors in variables

--- F_backend/urls.py
+++ F_backend/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/', include('accoint.urls')),
+    path('api/', include('account.urls')),
     path('admin/', admin.site.urls),
 ]
--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPAirView, TokenRefreshView
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+ 
 from . import api
 
 urlpatterns = [
     path('sipnup/',api.signup, name='signup'),
-    path('login/', TokenObtainPAirView.as_view(), name='token_obtain'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain'),
     path('login/', TokenRefreshView.as_view(), name='token_refresh')
 ]
