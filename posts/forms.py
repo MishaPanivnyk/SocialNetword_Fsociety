c33ce1d61ede535b80608909da30616bc18dab2b
OleksandrKuchera <leato44eek@gmail.com>
2024-04-23 12:45:21 +0300

    fа

--- posts/forms.py
+++ posts/forms.py
@@ -10,6 +10,6 @@ class PostForm(forms.ModelForm):
 
     def clean_description(self):
         description = self.cleaned_data['description']
-        if len(description) > 30:
-            raise forms.ValidationError("Description must be at least 30 characters long.")
+        if len(description) > 500:
+            raise forms.ValidationError("Description must be at least 500 characters long.")
         return description
